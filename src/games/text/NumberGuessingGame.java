6cf51bc5e14d304fd5fe37e787d561cb9bc839ee
Alonso del Arte <alonso.delarte@gmail.com>
2022-06-03 17:48:56 -0400

    Project 2 (no exception handling, no testing)

---
+++ src/games/text/NumberGuessingGame.java
@@ -0,0 +1,73 @@
+package games.text;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static final int MINIMUM_NUMBER = 1;
+
+    public static final int MAXIMUM_NUMBER = 20;
+
+    public static final int SPAN = MAXIMUM_NUMBER - MINIMUM_NUMBER;
+
+    public static final int MAXIMUM_GUESS_COUNT = 6;
+
+    private static Scanner scanner;
+
+    private static String playerName;
+
+    private static int thinkOfNumber() {
+        return (int) Math.floor(Math.random() * SPAN) + MINIMUM_NUMBER;
+    }
+
+    private static void playRound() {
+        int number = thinkOfNumber();
+        System.out.println("Well, " + playerName
+                + ", I'm thinking of a number between " + MINIMUM_NUMBER
+                + " and " + MAXIMUM_NUMBER);
+        int guessCount = 0;
+        boolean guessedIt = false;
+        while (guessCount < MAXIMUM_GUESS_COUNT && !guessedIt) {
+            System.out.println("Take a guess.");
+            int guess = scanner.nextInt();
+            guessCount++;
+            int comparison = Integer.signum(Integer.compare(guess, number));
+            switch (comparison) {
+                case -1:
+                    System.out.println("Your guess is too low");
+                    break;
+                case 0:
+                    guessedIt = true;
+                    System.out.println("Good job, " + playerName
+                            + "! You guessed my number in " + guessCount + " guesses!");
+                    break;
+                case 1:
+                    System.out.println("Your guess is too high");
+                    break;
+                default:
+                    String excMsg = "Unexpected comparison " + comparison;
+                    throw new NoSuchElementException(excMsg);
+            }
+        }
+        if (!guessedIt) {
+            System.out.println("Sorry, the number was " + number);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Number Guessing Game");
+        scanner = new Scanner(System.in);
+        System.out.println("Hello! What is your name?");
+        playerName = scanner.next();
+        boolean keepPlaying = true;
+        while (keepPlaying) {
+            playRound();
+            System.out.println("Would you like to play again? (y/n)");
+            String response = scanner.next().toLowerCase();
+            keepPlaying = response.startsWith("y");
+        }
+        scanner.close();
+    }
+
+}

bcae312cd07b4eed39b08c841947c018fbaab643
Alonso del Arte <alonso.delarte@gmail.com>
2022-06-08 16:42:49 -0400

    Passing invalid parameter tests

--- src/randomness/RandomDotOrgAccess.java
+++ src/randomness/RandomDotOrgAccess.java
@@ -13,9 +13,35 @@ import java.util.Scanner;
  */
 class RandomDotOrgAccess extends ExternalRandomnessProvider {
 
+    private static final int MINIMUM_AMOUNT = 1;
+
+    private static final int MAXIMUM_AMOUNT = 10000;
+
+    private static final int MINIMUM_MINIMUM = -1000000000;
+
+    private static final int MAXIMUM_MAXIMUM = 1000000000;
+
     @Override
     public int[] giveNumbers(int amount, int minimum, int maximum)
             throws IOException {
+        if (amount < MINIMUM_AMOUNT || amount > MAXIMUM_AMOUNT) {
+            String excMsg = "amount should be in range " + MINIMUM_AMOUNT
+                    + " to " + MAXIMUM_AMOUNT;
+            throw new IllegalArgumentException(excMsg);
+        }
+        if (minimum < MINIMUM_MINIMUM) {
+            String excMsg = "minimum should be at least " + MINIMUM_MINIMUM;
+            throw new IllegalArgumentException(excMsg);
+        }
+        if (maximum > MAXIMUM_MAXIMUM) {
+            String excMsg = "maximum should be at no more than "
+                    + MAXIMUM_MAXIMUM;
+            throw new IllegalArgumentException(excMsg);
+        }
+        if (minimum >= maximum) {
+            String excMsg = "minimum should not be greater than maximum";
+            throw new IllegalArgumentException(excMsg);
+        }
         return new int[amount];
     }
 

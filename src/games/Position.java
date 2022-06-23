0fae727331eca22742dfd22927202d6574e55542
Alonso del Arte <alonso.delarte@gmail.com>
2022-06-23 10:33:08 -0400

    Passing toString( ) test, prepping for equality, hash code tests

--- src/games/Position.java
+++ src/games/Position.java
@@ -4,6 +4,23 @@ public class Position {
 
     private final int coordX, coordY;
 
+    @Override
+    public String toString() {
+        return "(" + this.coordX + ", " + this.coordY + ")";
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
     // TODO: Write tests for this
     public Position offset(RelativePosition adjustment) {
         return this;
